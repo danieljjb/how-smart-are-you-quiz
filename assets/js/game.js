11c61f04377091d705c3b042e346fef16db03b75
danieljjb <sarshill@gmail.com>
2022-07-19 18:10:35 +0000

    Add variables to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,4 +5,11 @@ const answers = Array.from(document.querySelectorAll('.answer'));
 const questionCounterText = document.querySelector('#questionCounter');
 const scoreText = document.querySelector('#score');
 
+// Variables
 
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = [];
+let questionCounter = [];
+let availableQuestions = [];
+let questions = [];
