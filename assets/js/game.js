44eae2deb8c97c84255b92646312fc1b316714a5
danieljjb <sarshill@gmail.com>
2022-07-19 18:04:09 +0000

    Add query selector heading to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,8 @@
+// Query Selectors
+
 const question = document.querySelector('#question');
 const answers = Array.from(document.querySelectorAll('.answer'));
 const questionCounterText = document.querySelector('#questionCounter');
 const scoreText = document.querySelector('#score');
+
+
