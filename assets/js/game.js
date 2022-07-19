3df8902ea5bd3dd5f3e76a92c5798616aa604ff3
danieljjb <sarshill@gmail.com>
2022-07-19 18:01:52 +0000

    Add query selectors to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,4 @@
 const question = document.querySelector('#question');
+const answers = Array.from(document.querySelectorAll('.answer'));
+const questionCounterText = document.querySelector('#questionCounter');
+const scoreText = document.querySelector('#score');
