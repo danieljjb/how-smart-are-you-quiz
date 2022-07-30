16c4ac30a4b639830b2430b9327021c3f10c07d0
danieljjb <sarshill@gmail.com>
2022-07-30 15:05:07 +0000

    Remove unused variable from end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -10,7 +10,6 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
  
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
